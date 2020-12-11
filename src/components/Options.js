22876538f8c948da9680c85a60c8ff289a62dc81
Lars-August Udnesseter Johnson <Lars-August@lars-augusts-macbook-pro.home>
2020-12-11 22:14:21 +0100

    finished webpack, and babel

---
+++ src/components/Options.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Option from "../components/Option";
+
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}> Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started</p>}
+      {props.options.map((option) => (
+        <Option
+          key={option}
+          optionText={option}
+          handleDeleteSingleOption={props.handleDeleteSingleOption}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Options;
