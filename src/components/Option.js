260757d5b28b5a3d027fa5a7a0899b7516b1a249
Lars-August Udnesseter Johnson <Lars-August@lars-augusts-macbook-pro.home>
2020-12-16 12:27:07 +0100

    finished refactoring the components

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,19 +1,15 @@
 import React from "react";
 
-const Option = (props) => {
-  return (
-    <div>
-      <br />
-      <div style={{ display: "inline-block" }}>{props.optionText}</div>
-      <div style={{ display: "inline-block" }}>
-        <button
-          onClick={(e) => props.handleDeleteSingleOption(props.optionText)}
-        >
-          Remove
-        </button>
-      </div>
+const Option = (props) => (
+  <div>
+    <br />
+    <div style={{ display: "inline-block" }}>{props.optionText}</div>
+    <div style={{ display: "inline-block" }}>
+      <button onClick={(e) => props.handleDeleteSingleOption(props.optionText)}>
+        Remove
+      </button>
     </div>
-  );
-};
+  </div>
+);
 
 export default Option;
